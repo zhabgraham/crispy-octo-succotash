57c37e5e2b3e165f6a13984d347645b92a7c6c50
zhabgraham <artdraw5555@gmail.com>
2022-10-07 10:52:31 +0300

    1

---
+++ Lab_02/Lab_02/Lab_02.cpp
@@ -0,0 +1,30 @@
+// Lab_02.cpp
+// Чипігіна Анастасія
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 23
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a; // вхідний параметр
+    double z1; // результат обчислення 1-го виразу
+    double z2; // результат обчислення 2-го виразу
+    double z3;
+    double z;
+    double z5;
+
+    cout << "a = "; cin >> a;
+
+    z1 = (sin(2*a)+sin(5*a)-sin(3*a))/(cos(a)+1-2*sin(2*a)*sin(2*a));
+    z2 = 2 * sin(a);
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
+}
